b1c876c70d8d89cfd60697e0b309748c6ddd87fe
eduardokobata <eduardokmarto@gmail.com>
2026-07-07 23:14:45 -0300

    feat: implements LocalCatalogService and OstLoader for local caching

--- tabletop-rpg-soundtrack/src/main/java/br/org/tabletoprpg/soundtrack/service/catalog/LocalCatalogService.java
+++ tabletop-rpg-soundtrack/src/main/java/br/org/tabletoprpg/soundtrack/service/catalog/LocalCatalogService.java
@@ -1,6 +1,21 @@
 package br.org.tabletoprpg.soundtrack.service.catalog;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+import br.org.tabletoprpg.soundtrack.exception.TabletopExeption;
+import br.org.tabletoprpg.soundtrack.exception.DownloadError;
+import br.org.tabletoprpg.soundtrack.exception.OstNotFoundError;
+
+
+
 
 /**
  * Os arquivos da OST são armazenados na pasta local_storage.
@@ -9,6 +24,7 @@ import java.util.Collection;
 public class LocalCatalogService implements CatalogService {
 
     private final String localStoragePath;
+    private final String cachePath = "./cache/";
 
     public LocalCatalogService(String localStoragePath) {
         this.localStoragePath = localStoragePath;
@@ -18,16 +34,78 @@ public class LocalCatalogService implements CatalogService {
     public Collection<String> listAvailableOsts() {
         // Retorna os nomes das pastas dentro do diretório localStoragePath e retorna
         // como uma coleção de strings.
-        throw new UnsupportedOperationException("Not implemented yet");
+        
+        Path rootPath = getLocalPath();
+
+         try (Stream<Path> paths = Files.list(rootPath)) {
+            return paths.filter(Files::isDirectory).map(path -> path.getFileName().toString()).collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new TabletopExeption("Erro ao listar OSTs");
+        }
+
+
     }
 
     @Override
     public String downloadOst(String ostName) {
         // Copia todos os arquivos da pasta localStoragePath/ostName para o cache local
         // e retorna o caminho do cache local.
-        // throw new UnsupportedOperationException("Not implemented yet");
+
+        Path sourcePath = Paths.get(localStoragePath, ostName);
+        Path targetPath = Paths.get(cachePath, ostName);
+
+        if (!Files.exists(sourcePath)) {
+            throw new OstNotFoundError("A OST" + ostName + " não foi encontrada");
+        }
+
+        try (Stream<Path> stream = Files.walk(sourcePath)) { //entra na pasta de origem da OST
+            
+            stream.forEach(source -> {
+                try {
+                    Path destination = targetPath.resolve(sourcePath.relativize(source)); //entra em ./cache/
+                    
+                    //se for um diretório, temos que cria-lo no cache antes de copiarmos arquivos para eles
+                    //se for um arquivo, apenas os copiamos
+
+                    if (Files.isDirectory(source)) {
+                        if (!Files.exists(destination)) {
+                            Files.createDirectory(destination);
+
+                        }
+
+                    } else {
+                        Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING); //Copia todos os arquivos da pasta localStoragePath/ostName para o cache local
+                    }
+                } catch (IOException ex) {
+                    throw new DownloadError("falha na copia da OST");
+                }
+            });
+
+        } catch (IOException e) {
+            throw new TabletopExeption("erro ao realizar o cache local da OST");
+        }
+
         System.out.println("Downloading OST: " + ostName);
-        return "./cache/" + ostName;
+        return targetPath.toString();
+        
+
+    }
+
+    private Path getLocalPath() {
+        try {
+            Path rootPath = Paths.get(localStoragePath);
+
+            if (!Files.exists(rootPath)) {
+                throw new TabletopExeption("O diretório do storage local nao encontrado " + localStoragePath);
+            }
+
+            return rootPath;
+
+        } catch (Exception e) {
+           throw new TabletopExeption("Path local nao encontrado");
+        }
+
+        
 
     }
 
--- tabletop-rpg-soundtrack/src/main/java/br/org/tabletoprpg/soundtrack/service/loader/OstLoader.java
+++ tabletop-rpg-soundtrack/src/main/java/br/org/tabletoprpg/soundtrack/service/loader/OstLoader.java
@@ -1,15 +1,51 @@
 package br.org.tabletoprpg.soundtrack.service.loader;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 import br.org.tabletoprpg.soundtrack.model.Ost;
+import br.org.tabletoprpg.soundtrack.model.Theme;
 
 /**
  * 
  * Transformar uma OST armazenada localmente em objetos do domínio (model).
  */
-public class OstLoader {
+public class OstLoader implements OstLoaderInterface {
 
-    public Ost load(String ostName) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    @Override
+    public Ost load(String localPath) { //o SessionService exige localPath, não ostName
+
+        
+        Path manifestPath = Paths.get(localPath, "manifest.json");
+
+        if (!Files.exists(manifestPath)) {
+            throw new RuntimeException("manifest.json não encontrado em");
+        }
+
+        try {
+            String jsonContent = Files.readString(manifestPath);
+            return parseJsonToOst(jsonContent);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao ler manifest.json");
+        }
+
+
     }
+
+    //TODO: Fazer o parsing do json manifest da forma correta
+    private Ost parseJsonToOst(String json) {
+
+        String ostName = null;
+        List<Theme> themes = null;
+
+        return new Ost(ostName, themes); //arrumar depois com o formato certo do objeto Ost
+    }
+    
+
+
 
 }
